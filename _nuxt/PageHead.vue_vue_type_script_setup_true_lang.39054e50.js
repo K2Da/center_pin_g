b82084e8232b00bb293ce85d61ebaab4d219083d
K2Da <id.k2da@gmail.com>
2022-09-30 15:29:42 +0900

    width

---
+++ _nuxt/PageHead.vue_vue_type_script_setup_true_lang.39054e50.js
@@ -0,0 +1,1 @@
+import{a as l,G as c,H as t,o as i,c as r,B as a,q as _,h as p,t as d}from"./entry.38f1a035.js";const g=l({__name:"PageHead",props:{title:null},setup(e){const{title:o}=e;return c(o),(m,u)=>{const n=t("Title"),s=t("Head");return i(),r(s,null,{default:a(()=>[_(n,null,{default:a(()=>[p(d(`Cp(g): ${e.title}`),1)]),_:1})]),_:1})}}});export{g as _};
