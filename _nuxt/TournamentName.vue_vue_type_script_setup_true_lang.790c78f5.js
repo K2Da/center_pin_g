6c7f60dee2cd7188b14c1a35290023810b6a797e
K2Da <id.k2da@gmail.com>
2022-09-30 14:16:27 +0900

    member

---
+++ _nuxt/TournamentName.vue_vue_type_script_setup_true_lang.790c78f5.js
@@ -0,0 +1,1 @@
+import{a as n,o as a,c as o,B as r,h as m,t as c,I as s}from"./entry.b80b6c23.js";const p=n({__name:"TournamentName",props:{tournamentKey:null,name:null},setup(t){return(u,_)=>{const e=s;return a(),o(e,{to:{name:"tournament-detail",query:{t:t.tournamentKey}},"no-prefetch":""},{default:r(()=>[m(c(t.name),1)]),_:1},8,["to"])}}});export{p as _};
