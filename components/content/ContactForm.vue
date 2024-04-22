d19f451e3fd18b2824abfced887096187b5cf16c
K2Da <id.k2da@gmail.com>
2024-04-22 15:58:39 +0900

    Update ContactForm.vue and about.vue components

--- components/content/ContactForm.vue
+++ components/content/ContactForm.vue
@@ -9,6 +9,8 @@ function send() {
 </script>
 
 <template>
-  <textarea v-model="text" />
-  <button :disabled="text.length == 0" v-on:click="send">送信</button>
+  <UTextarea v-model="text" class="mx-4 my-2" />
+  <UButton :disabled="text.length == 0" v-on:click="send" class="m-4"
+    >送信</UButton
+  >
 </template>
