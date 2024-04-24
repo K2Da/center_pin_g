71fcca57a16b3569058b8569db20722ddf3e55d3
K2Da <id.k2da@gmail.com>
2024-04-25 04:25:39 +0900

    Update NaviLink.vue and about.md files

--- components/NaviLink.vue
+++ components/NaviLink.vue
@@ -3,7 +3,7 @@
   <nav class="p-1 text-base">
     <NuxtLink :to="{ name: 'index' }">CP(g)</NuxtLink> |
     <NuxtLink :to="{ name: 'topics' }">Topic</NuxtLink> |
-    <NuxtLink :to="{ name: 'calendar' }">Cal</NuxtLink> |
+    <NuxtLink :to="{ name: 'calendar' }">Calendar</NuxtLink> |
     <NuxtLink :to="{ name: 'tournaments' }">Tourney</NuxtLink> |
     <NuxtLink :to="{ name: 'players' }">Player</NuxtLink> |
     <NuxtLink :to="{ name: 'teams' }">Team</NuxtLink> |
