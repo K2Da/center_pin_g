b617ddf5f5109d44ff4cfbfbb09632cf646d7532
K2Da <id.k2da@gmail.com>
2024-04-30 04:55:40 +0900

    Refactor topic list component to use pagination and async data fetching

--- components/topic/list.vue
+++ components/topic/list.vue
@@ -1,13 +1,37 @@
 <script setup lang="ts">
-const { data } = await useAsyncData(() => {
+const ITEM_COUNT = 5;
+const pageNoRef = ref(1);
+const changePageNo = (v: number) => {
+  pageNoRef.value = v;
+  window.scroll({ top: 0, left: 0 });
+};
+
+const { data } = await useAsyncData(
+  `topic_page_${pageNoRef.value}`,
+  () => {
+    console.log('fetch');
+    return queryContent('/topic')
+      .where({ updated: { $exists: true } })
+      .sort({ updated: -1 })
+      .skip((pageNoRef.value - 1) * ITEM_COUNT)
+      .limit(ITEM_COUNT)
+      .find();
+  },
+  { watch: [pageNoRef] },
+);
+const { data: total } = await useAsyncData(() => {
   return queryContent('/topic')
     .where({ updated: { $exists: true } })
-    .sort({ updated: -1 })
-    .find();
+    .count();
 });
 </script>
 
 <template>
+  <ListPager
+    :pageNo="pageNoRef"
+    :maxPage="Math.ceil((total || 0) / ITEM_COUNT)"
+    @change="changePageNo"
+  />
   <div v-for="topic in data" :key="topic._path">
     <TopicExcerpt :topic />
   </div>
